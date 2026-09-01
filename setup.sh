938f3b0532de548504009f631d86be0e1c966c76
Melfritz38 <244699431+Melfritz38@users.noreply.github.com>
2026-09-01 20:32:10 +0000

    Fix IBSIMU_GIT_ID definition in setup script

--- setup.sh
+++ setup.sh
@@ -3,16 +3,26 @@ set -e
 
 echo "=== System- und Python-Pakete installieren ==="
 sudo apt-get update
-sudo apt-get install -y build-essential gfortran libgsl-dev libgtkmm-3.0-dev libcairo2-dev libpng-dev python3-pip python3-numpy python3-scipy python3-matplotlib automake libtool git pkg-config
+sudo apt-get install -y build-essential gfortran libgsl-dev libgtkmm-3.0-dev libcairo2-dev libpng-dev python3-pip python3-numpy python3-scipy python3-matplotlib automake libtool git pkg-config autoconf
 
 echo "=== IBSimu Quellcode holen & installieren ==="
-if [ ! -d "ibsimu_src" ]; then
-    git clone https://git.code.sf.net/p/ibsimu/code ibsimu_src
-fi
+rm -rf ibsimu_src
+git clone https://git.code.sf.net/p/ibsimu/code ibsimu_src
 
 cd ibsimu_src
-./autogen.sh
+touch ChangeLog NEWS README AUTHORS
+autoreconf -i --force
 ./configure
+
+# Alle geforderten Makros in src/id.hpp schreiben
+cat << 'IDEOF' > src/id.hpp
+#ifndef ID_HPP
+#define ID_HPP
+#define IBSIMU_VERSION "1.0.6"
+#define IBSIMU_GIT_ID "git"
+#endif
+IDEOF
+
 make -j$(nproc)
 sudo make install
 sudo ldconfig
